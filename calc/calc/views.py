4833562843ad03b5f057cb8c31d5ef810a20cbfa
RahulMirji <121561908+RahulMirji@users.noreply.github.com>
2024-10-21 20:26:34 +0530

    calculator updated

--- calc/calc/views.py
+++ calc/calc/views.py
@@ -1,6 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from views import clculator
+
 
 def calculator(request):
-    return render(request, HttpResponse "calculator.html")
+    c=''
+    try:
+        if request.method=='POST':
+            n1=eval(request.POST.get('num1'))
+            n2=eval(request.POST.get('num2'))
+            opr=request.POST.get('opr')
+            if opr=="+":
+                c=n1+n2
+            elif opr=="-":
+                c=n1-n2
+            elif opr=="*":
+                c=n1*n2
+            elif opr=="/":
+                c=n1/n2
+    except:
+        c="Invalid Opr"
+    return render(request,"calculator.html",{'c':c})
